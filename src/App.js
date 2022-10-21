330366d03bf39d955f10ed17a8913ea62e0e6ee0
yuhongz12 <yuhong.zhao12@gmail.com>
2022-10-21 11:54:05 -0400

    modify app.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <div>
+      <h1>Food Algorithms</h1>
+
+      <h3>Navigation</h3>
+      <ul>
+        <li>Home</li>
+        <li>Recommendation</li>
+        <li>Trending</li>
+        <li>Search</li>
+        <li>Pofrile</li>
+      </ul>
+
+      <h3>Profile options</h3>
+      <ul>
+        <li>Login</li>
+        <li>Profile</li>
+        <li>Sign out</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
