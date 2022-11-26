0cd4e401673c7b22f9a390f1099d4f71a36386f8
yuhongz12 <yuhong.zhao12@gmail.com>
2022-11-26 16:47:23 -0500

    refactor home page with bootstrap-react compoenents

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -1,48 +1,65 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import Dropdown from "react-bootstrap/Dropdown";
-import DropdownButton from "react-bootstrap/DropdownButton";
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+const NavbarComponent = () => {
 
     return(
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to={"/home"} >FoodAlgorithms</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to={'/search'}>Recipes</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/search">Cocktails</Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                               aria-expanded="false">
-                                Profile
-                            </Link>
-                            <ul className="dropdown-menu">
-                                <li><Link className="dropdown-item" href="#">Login</Link></li>
-                                <li><Link className="dropdown-item" href="#">Register</Link></li>
-                            </ul>
-                        </li>
-
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        // <nav className="navbar navbar-expand-lg bg-light">
+        //     <div className="container-fluid">
+        //         <Link className="navbar-brand" to={"/home"} >FoodAlgorithms</Link>
+        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+        //                 data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
+        //                 aria-label="Toggle navigation">
+        //             <span className="navbar-toggler-icon"></span>
+        //         </button>
+        //         <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        //             <ul className="navbar-nav">
+        //                 <li className="nav-item">
+        //                     <Link className="nav-link active" aria-current="page" to={'/'}>Home</Link>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <Link className="nav-link active" aria-current="page" to={'/search'}>Recipes</Link>
+        //                 </li>
+        //                 <li className="nav-item">
+        //                     <Link className="nav-link" to="/search">Cocktails</Link>
+        //                 </li>
+        //                 <li className="nav-item dropdown">
+        //                     <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        //                        aria-expanded="false">
+        //                         Profile
+        //                     </Link>
+        //                     <ul className="dropdown-menu">
+        //                         <li><Link className="dropdown-item" href="#">Login</Link></li>
+        //                         <li><Link className="dropdown-item" href="#">Register</Link></li>
+        //                     </ul>
+        //                 </li>
+        //
+        //
+        //             </ul>
+        //         </div>
+        //     </div>
+        // </nav>
+        <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="/">FoodAlgorithms</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href={'/search'}>Recipes</Nav.Link>
+                        <Nav.Link href={'/search'}>Cocktails</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Login</Nav.Link>
+                        <Nav.Link href="#deets">Register</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
 
     )
 }
 
-export default Navbar;
+export default NavbarComponent;
--- src/random-recipes/index.js
+++ src/random-recipes/index.js
@@ -1,38 +1,83 @@
 import React from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const RandomRecipes = () => {
     return(
-        <div className="row">
-            <div className="col-sm-4">
-                <div className="card">
-                    <img src="https://www.themealdb.com/images/media/meals/uuxwvq1483907861.jpg" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Eton Mess</h5>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4">
-                <div className="card">
-                    <div className="card">
-                        <img src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" className="float-start" alt="..."/>
-                        <div className="card-body float-end">
-                            <p className="card-title">Eton Mess</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-sm-4">
-                <div className="card">
-                    <div className="card">
-                        <img src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" className="float-start" alt="..."/>
-                        <div className="card-body float-end">
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk
-                                of the card's content.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        // <div className="row">
+        //     <div className="col-sm-4">
+        //         <div className="card">
+        //             <img src="https://www.themealdb.com/images/media/meals/uuxwvq1483907861.jpg" alt="..."/>
+        //             <div className="card-body">
+        //                 <h5 className="card-title">Eton Mess</h5>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className="col-sm-4">
+        //         <div className="card">
+        //             <div className="card">
+        //                 <img src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" className="float-start" alt="..."/>
+        //                 <div className="card-body float-end">
+        //                     <p className="card-title">Eton Mess</p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className="col-sm-4">
+        //         <div className="card">
+        //             <div className="card">
+        //                 <img src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" className="float-start" alt="..."/>
+        //                 <div className="card-body float-end">
+        //                     <p className="card-text">Some quick example text to build on the card title and make up the bulk
+        //                         of the card's content.</p>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <Row>
+            <Col sm={'6'} md={'6'} lg="4">
+                    <Card>
+                        <Card.Img variant="top" src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit
+                                longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            <Col sm={'6'} md={'6'} lg="4">
+                <Card>
+                    <Card.Img variant="top" src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col lg="4" className="d-none d-lg-block">
+                <Card>
+                    <Card.Img variant="top" src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/4:3/w_3552,h_2664,c_limit/ba-tikka-masala-2.jpg" />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a longer card with supporting text below as a natural
+                            lead-in to additional content. This content is a little bit
+                            longer.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
     )
 }
 
