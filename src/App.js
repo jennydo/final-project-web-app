785f703314cbe63df3a2a3bb2f74d4f41fb8d0f2
yuhongz12 <yuhong.zhao12@gmail.com>
2022-11-26 21:51:26 -0500

    create meal details page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import {Route, Routes} from "react-router";
 import Home from "./home";
 import Navbar from "./navbar";
 import Search from "./search";
+import MealDetails from "./meal-details";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
       <BrowserRouter>
                 <Navbar/>
+          <Container>
               <Routes>
                   <Route path="/*"
                          element={<Home/>}/>
                   <Route path="/search"
                          element={<Search/>}/>
+                  <Route path="/meal/details/:fid"
+                         element={<MealDetails/>}/>
               </Routes>
+          </Container>
       </BrowserRouter>
   );
 }
