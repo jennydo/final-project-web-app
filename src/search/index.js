3f88c2f48708df531e20ec0a2ed750f5638a5686
yuhongz12 <yuhong.zhao12@gmail.com>
2022-12-02 17:30:17 -0500

    refactor search screen with meal cards

--- src/search/index.js
+++ src/search/index.js
@@ -1,6 +1,8 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {findFoodBySearchTermThunk} from "./search-thunks";
+import MealCard from "../meal-card/mealCard";
+import Row from "react-bootstrap/Row";
 
 
 const Search = () => {
@@ -13,29 +15,27 @@ const Search = () => {
     return (
         <>
             <h1>Search</h1>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    <button
-                        className="btn btn-primary float-end"
-                        onClick={() => {
-                            dispatch(findFoodBySearchTermThunk(searchTerm))
-                        }}>Search
-                    </button>
-                    <input
-                        className="form-control w-75"
-                        onChange={(e) => {
-                            setSearchTerm(e.target.value)
-                        }}
-                        value={searchTerm}/>
-                </li>
-                {
-                    recipes && recipes.map((recipe) =>
-                                             <li key={recipe.idMeal} className="list-group-item">
-                                                 {recipe.strMeal}
-                                             </li>
-                           )
-                }
-            </ul>
+            <div>
+                <button
+                    className="btn btn-primary float-end"
+                    onClick={() => {
+                        dispatch(findFoodBySearchTermThunk(searchTerm))
+                    }}>Search
+                </button>
+                <input
+                    className="form-control w-75"
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value)
+                    }}
+                    value={searchTerm}/>
+            </div>
+                    <Row>
+                        {
+                            recipes && recipes.map(m => <MealCard meal={m}/>)
+                        }
+                    </Row>
+
+
         </>
     )
 }
