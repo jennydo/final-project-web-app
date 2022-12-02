92c7515eb834a4a3cdc9b3bcfe97a16a9b8b0721
yuhongz12 <yuhong.zhao12@gmail.com>
2022-12-02 17:24:23 -0500

    extract meal cards into seperate function

--- src/random-recipes/index.js
+++ src/random-recipes/index.js
@@ -7,6 +7,7 @@ import {Navigate, useNavigate} from 'react-router';
 
 import randomMealThunks from "./random-meal-thunks";
 import {Link} from "react-router-dom";
+import MealCard from "../meal-card/mealCard";
 
 const RandomRecipes = () => {
 
@@ -20,25 +21,7 @@ const RandomRecipes = () => {
 
     return(
         <Row>
-            { recipes.map( r =>
-
-            <Col sm={'6'} md={'6'} lg="3">
-                <div onClick={() => {
-                    navigate('/meal/details/' + r.idMeal)
-                }}>
-                    <Card>
-                        <Card.Img variant="top" src={r.strMealThumb} />
-                        <Card.Body>
-                            <Card.Title>{r.strMeal}</Card.Title>
-                            <Card.Text>
-                                <h5><span className="badge bg-secondary">{r.strArea}</span> <span
-                                    className="badge bg-secondary">{r.strCategory}</span></h5>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-
-                </Col>
+            { recipes.map( r => <MealCard meal={r}/>
             )}
         </Row>
     )
