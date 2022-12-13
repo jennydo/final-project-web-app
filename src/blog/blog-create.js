d47af29b90fadf3c95ed845a815f8c71d708b47f
yuhongz12 <yuhong.zhao12@gmail.com>
2022-12-13 18:44:43 -0500

    update blog page home page

--- src/blog/blog-create.js
+++ src/blog/blog-create.js
@@ -1,9 +1,15 @@
 import {useState} from "react";
 import ReactMarkdown from "react-markdown";
 import {useDispatch} from "react-redux";
-import {Button} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import {createBlogThunk} from "./blog-thunks";
 import {useNavigate} from "react-router";
+import {Link} from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const BlogCreate = () => {
     const [title, setTitle] = useState("");
@@ -24,21 +30,62 @@ const BlogCreate = () => {
     }
     return(
         <div>
-            <form>
-
-                <input value={title} placeholder={'Blog title'} onChange={(event) => setTitle(event.target.value)}/>
-
-                <textarea value={summary} placeholder={'Blog body'} onChange={(event) => setSummary(event.target.value)}/>
-            </form>
+            <Link to={'/blog'} className={'text-decoration-none text-secondary'}>
+                <i className="bi bi-arrow-left me-1"></i>Back
+            </Link>
+            <h2>Write a blog</h2>
 
 
-            <ReactMarkdown children={summary}/>
+            <Container>
+                <Row className={'mb-3 mt-2'} >
+                    <Col md={6} >
+                        <span className={'text-muted'}>Write your blog here</span>
+                        <Form>
+                            <FloatingLabel controlId="blogTitle"
+                                           label="Title"
+                                           className="mb-3"
+                            >
+                                <Form.Control
+                                    type="text"
+                                    value={title}
+                                    placeholder="Title"
+                                onChange={(event) => setTitle(event.target.value)}/>
+                            </FloatingLabel>
+                            <FloatingLabel controlId="blogBody" label="Body">
+                                <Form.Control
+                                    as="textarea"
+                                    placeholder="Body"
+                                    style={{ height: '15rem' }}
+                                    value={summary}
+                                    onChange={(event) => setSummary(event.target.value)}
+                                />
+                            </FloatingLabel>
+                            <Form.Text id="passwordHelpBlock" muted>
+                                Write your blog using Markdown.</Form.Text>
+                        </Form>
+
+                    </Col>
+
+                    <Col>
+                        <hr className={'d-block d-md-none'} />
+                        <span className={'text-muted'}>Preview</span>
+                        <h3>{title}</h3>
+                        <ReactMarkdown children={summary}/>
+                    </Col>
+                </Row>
+
+                <Button onClick={createBlog} className={'mb-3'}>Publish Blog</Button>
 
 
-            <Button onClick={createBlog}>Create Blog</Button>
 
 
-        </div>)
-}
 
-export default  BlogCreate;
+
+
+            </Container>
+
+
+        </div>
+)}
+
+export default BlogCreate;
