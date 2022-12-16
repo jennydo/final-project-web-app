678da588e777029a7ed30f2d0efb258ffa51dbe1
Jenny <dolientrang@gmail.com>
2022-12-16 09:16:47 +0700

    update project based on requirements in the checklist. added blogs and modified follows in public-profile

--- src/home/index.js
+++ src/home/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import RandomRecipes from "../random-recipes";
 import RandomCocktails from "../random-cocktails";
+import {useSelector} from "react-redux";
+import Follows from "../follows/follows";
 
 const Home = () => {
+    const {currentUser} = useSelector((state) => state.users)
+
     return(
         <div>
 
+
             <div className={'container'}>
+
+                {
+                    currentUser &&
+                    <h1>Welcome new user: {currentUser.username}</h1>
+                }
+
                 <h3>Recommended Recipes</h3>
                 <RandomRecipes/>
                 {/*<h3>Recommended Cocktails</h3>*/}
@@ -15,8 +26,9 @@ const Home = () => {
 
             <p>
                 {}
-
             </p>
+
+
 
         </div>
     )
